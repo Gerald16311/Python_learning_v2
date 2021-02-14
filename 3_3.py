5f5ed62a433bf406bd412620624401902d06fb21
Ilia Marchukov <Gerald16311@gmail.com>
2021-02-14 21:36:59 +0400

    3_3 Done

--- 3_3.py
+++ 3_3.py
@@ -7,15 +7,19 @@
 # }
 # Подумайте: полезен ли будет вам оператор распаковки?
 # Как поступить, если потребуется сортировка по ключам? Можно ли использовать словарь в этом случае
+
+
 users_dict = {}
 
 
-def thesaurus(name):
-    users_dict.update({name[0]: name})
+def thesaurus(name1, name2, name3, name4):
+    names_list = [name1, name2, name3, name4]
+    for name in names_list:
+        if name[0] in users_dict:
+            users_dict[name[0]].append(name)
+        else:
+            users_dict[name[0]] = [name]
     return users_dict
 
 
-print(thesaurus("Иван"))
-print(thesaurus("Мария"))
-print(thesaurus("Петр"))
-print(thesaurus("Илья"))
+print(thesaurus("Иван", "Мария", "Петр", "Илья"))
