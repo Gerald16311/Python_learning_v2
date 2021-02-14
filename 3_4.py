65f7c6b33f550db0d98ecad3b46704a355057759
Ilia Marchukov <Gerald16311@gmail.com>
2021-02-14 21:43:51 +0400

    3_4 in action

--- 3_4.py
+++ 3_4.py
@@ -12,3 +12,20 @@
 #     }
 # }
 # Как поступить, если потребуется сортировка по ключам?
+users_dict = {}
+
+
+def thesaurus_adv(name1, name2, name3, name4, name5):
+    names_list = [name1.split(" "), name2.split(" "), name3.split(" "), name4.split(" "), name5.split(" ")]
+    print(names_list)
+    print(names_list[0][1][0])
+
+    # for name in names_list:
+    #     if name[0][1][0] in users_dict:
+    #         users_dict[name[0]].append(name)
+    #     else:
+    #         users_dict[name[0]] = [" ".join(name[0])]
+    # return users_dict
+
+
+print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"))
