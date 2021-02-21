21115cce97b13d4fcd790e6d2524d43010cfb516
Ilia Marchukov <Gerald16311@gmail.com>
2021-02-21 15:27:29 +0400

    4_5 done

--- 4_5.py
+++ 4_5.py
@@ -1,3 +1,23 @@
 # *(вместо 4) Доработать скрипт из предыдущего задания: теперь он должен работать и из консоли. Например:
 # > python task_4_5.py USD
 # 75.18, 2020-09-05
+import requests
+import xml.etree.ElementTree as ET
+from datetime import datetime
+
+
+def currency_rates(argv):
+    program, *args = argv
+    r = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+    tree = ET.fromstring(r.text)
+    date_list = tree.attrib["Date"].split(".")
+    date = datetime(year=int(date_list[2]), month=int(date_list[1]), day=int(date_list[0]))
+    for i in range(0, len(tree)):
+        if tree[i][1].text == args[0].upper():
+            print(f"Курс валюты {round(float(tree[i][4].text.replace(',', '.')), 2)}, Дата {date.day}-{date.month}-{date.year}")
+
+
+if __name__ == '__main__':
+    import sys
+
+    exit(currency_rates(sys.argv))
