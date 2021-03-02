985edb2e711f82ed1add46d101d5073cfb24bde7
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-02 12:43:59 +0400

    5_2 Done

--- 5_2.py
+++ 5_2.py
@@ -1,1 +1,7 @@
 # *(вместо 1) Решить задачу генерации нечётных чисел от 1 до n (включительно), не используя ключевое слово yield.
+n = int(input("Введите до какого числа генерировать нечетные числа - "))
+nums_gen = (num for num in range(1, n+1, 2))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
+print(next(nums_gen))
