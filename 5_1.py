22869bdac91cbc396f32bfceb701e97fb1d8d8aa
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-02 12:42:04 +0400

    5_1 Done

--- 5_1.py
+++ 5_1.py
@@ -10,3 +10,20 @@
 # >>> next(odd_to_15)
 # ...StopIteration...
 #
+def odd_nums(n):
+    for num in range(1, n + 1, 2):
+        yield num
+
+
+n = int(input("Введите до какого числа генерировать нечетные числа - "))
+
+odd_to_n = odd_nums(n)
+
+print(next(odd_to_n))
+print(next(odd_to_n))
+print(next(odd_to_n))
+print(next(odd_to_n))
+print(next(odd_to_n))
+print(next(odd_to_n))
+print(next(odd_to_n))
+print(next(odd_to_n))
