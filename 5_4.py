bafc6441c9a6d1f216c0c9a81191051927036016
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-03 11:46:14 +0400

    5_4 Done

--- 5_4.py
+++ 5_4.py
@@ -7,10 +7,12 @@ src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 # памяти, по скорости.
 
 # Решение в лоб
-result = []
-for i in range(0, len(src)-1):
-    if src[i+1] > src[i]:
-        result.append(src[i+1])
+# result = []
+# for i in range(0, len(src)-1):
+#     if src[i+1] > src[i]:
+#         result.append(src[i+1])
+#
+# print(result)
 
+result = [src[i] for i in range(1, len(src)) if src[i] > src[i-1]]
 print(result)
-
