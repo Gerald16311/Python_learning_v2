5fbe9e5a769829b5d3a28afa157c3ddd356f96bd
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-05 13:29:18 +0400

    6_2 непонятная разница между while и for

--- 6_2.py
+++ 6_2.py
@@ -3,10 +3,14 @@
 # размер которых превышает объем ОЗУ компьютера.
 
 result = []
+
 with open('nginx_logs.txt', encoding='utf-8') as f:
-    for line in f:
-        content = f.readline().split()
+    line = f.readline()
+    while line:
+        content = line.split(" ")
         result.append(content[0])
+        line = f.readline()
+
 spam_ip = ""
 spam_ip_count = 0
 unic_set = set(result)
@@ -17,6 +21,22 @@ for ip in unic_set:
 
 print(f"IP адресс спамера {spam_ip}, количество запросов {spam_ip_count}")
 
+result_for = []
+
+with open('nginx_logs.txt', encoding='utf-8') as f:
+    for line in f:
+        content = f.readline().split()
+        result_for.append(content[0])
+
+spam_ip_for = ""
+spam_ip_count_for = 0
+unic_set_for = set(result_for)
+for ip in unic_set:
+    if result_for.count(ip) > spam_ip_count_for:
+        spam_ip_count_for = result_for.count(ip)
+        spam_ip_for = ip
+
+print(f"IP адресс спамера {spam_ip_for}, количество запросов {spam_ip_count_for}")
 
 # unic = []
 # all = []
