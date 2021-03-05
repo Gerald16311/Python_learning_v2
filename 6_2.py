824fa2a2a53d1472829b17f3fef3b9dadf68ae4f
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-05 12:41:55 +0400

    6_2 Done...

--- 6_2.py
+++ 6_2.py
@@ -7,12 +7,33 @@ with open('nginx_logs.txt', encoding='utf-8') as f:
     for line in f:
         content = f.readline().split()
         result.append(content[0])
-
-spam_ip = result[0]
-spam_ip_count = result.count(spam_ip)
-for ip in result:
+spam_ip = ""
+spam_ip_count = 0
+unic_set = set(result)
+for ip in unic_set:
     if result.count(ip) > spam_ip_count:
         spam_ip_count = result.count(ip)
         spam_ip = ip
 
 print(f"IP адресс спамера {spam_ip}, количество запросов {spam_ip_count}")
+
+
+# unic = []
+# all = []
+# max=0
+# with open('nginx_logs.txt', 'r') as f:
+#     line = f.readline()
+#     while line:
+#         ip = line.strip()[0:15]
+#         remote_address = ip.replace('-', '').replace(' ', '')
+#         if remote_address not in unic:
+#             unic.append(remote_address)
+#         all.append(remote_address)
+#         line = f.readline()
+#
+# for i in unic:
+#     if all.count(i) > max:
+#         max = all.count(i)
+#         spamip = i
+#
+# print('IP спамера: ', spamip, "Количество вхождений:", max)
