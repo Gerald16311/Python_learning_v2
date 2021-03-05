3f8162058a194a85f6bfd23db3ecf204f710d3d7
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-05 14:32:48 +0400

    6_3 не готово

--- 6_3.py
+++ 6_3.py
@@ -10,3 +10,28 @@
 # скалолазание,охота
 # горные лыжи
 #
+result = {}
+
+with open('users.csv', encoding='utf-8') as users_file:
+    with open('hobby.csv', encoding='utf-8') as hobby_file:
+        names_line = users_file.readline()
+        hobby_line = hobby_file.readline()
+        while names_line:
+            if not names_line:
+                print("1")
+                break
+            else:
+                names_line = users_file.readline()
+                user_name = names_line.replace("\n", "")
+
+        while hobby_line:
+            if not hobby_line:
+                user_hobby = "None"
+                hobby_line = hobby_file.readline()
+            else:
+                hobby_line = hobby_file.readline()
+                user_hobby = hobby_line.replace("\n", "")
+
+        result[user_name] = user_hobby
+
+print(result)
