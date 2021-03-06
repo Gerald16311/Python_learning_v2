6cb62319f94cb2cf175d8bf823c5f1198e84960b
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-06 16:43:40 +0400

    6_2 сделано со словарем

--- 6_2.py
+++ 6_2.py
@@ -21,19 +21,33 @@
 #
 # print(f"IP адресс спамера {spam_ip}, количество запросов {spam_ip_count}")
 
-result = []
+# result = []
+#
+# with open('nginx_logs.txt', encoding='utf-8') as f:
+#     for line in f:
+#         content = line.split()
+#         result.append(content[0])
+#
+# spam_ip = ""
+# spam_ip_count = 0
+# unic_set = set(result)
+# for ip in unic_set:
+#     if result.count(ip) > spam_ip_count:
+#         spam_ip_count_for = result.count(ip)
+#         spam_ip_for = ip
+#
+# print(f"IP адресс спамера {spam_ip}, количество запросов {spam_ip_count}")
+
+result = {}
 
 with open('nginx_logs.txt', encoding='utf-8') as f:
     for line in f:
         content = line.split()
-        result.append(content[0])
+        if content[0] in result:
+            result[content[0]] += 1
+        else:
+            result[content[0]] = 1
 
-spam_ip = ""
-spam_ip_count = 0
-unic_set = set(result)
-for ip in unic_set:
-    if result.count(ip) > spam_ip_count:
-        spam_ip_count_for = result.count(ip)
-        spam_ip_for = ip
+max_ip_count = max(result, key=result.get)
 
-print(f"IP адресс спамера {spam_ip}, количество запросов {spam_ip_count}")
+print(f"IP адресс спамера {max_ip_count}, количество запросов {result[max_ip_count]}")
