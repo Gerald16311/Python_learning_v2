23386b77a63554326c29d6ebb28be231162c5435
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-10 20:51:17 +0400

    7_2 done

--- 7_2.py
+++ 7_2.py
@@ -1,26 +1,10 @@
 # *(вместо 1) Написать скрипт, создающий из config.yaml стартер для проекта со следующей структурой:
-# |--my_project
-#    |--settings
-#    |  |--__init__.py
-#    |  |--dev.py
-#    |  |--prod.py
-#    |--mainapp
-#    |  |--__init__.py
-#    |  |--models.py
-#    |  |--views.py
-#    |  |--templates
-#    |     |--mainapp
-#    |        |--base.html
-#    |        |--index.html
-#    |--authapp
-#    |  |--__init__.py
-#    |  |--models.py
-#    |  |--views.py
-#    |  |--templates
-#    |     |--authapp
-#    |        |--base.html
-#    |        |--index.html
-#
-#
 # Примечание: структуру файла config.yaml придумайте сами, его можно создать в любом текстовом редакторе «руками» (не
 # программно); предусмотреть возможные исключительные ситуации, библиотеки использовать нельзя.
+import os
+with open('config.yaml') as f:
+    for line in f:
+        if not os.path.exists(os.path.dirname(line.strip())):
+            os.makedirs(os.path.dirname(line.strip()))
+        with open(f'{os.path.dirname(line.strip())}/{os.path.basename(line.strip())}', 'w') as item:
+            pass
