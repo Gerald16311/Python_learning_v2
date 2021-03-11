acb6c46731acb0097ba792551f38ed0ccf4e4e83
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-11 21:03:25 +0400

    7_4 Done

--- 7_4.py
+++ 7_4.py
@@ -11,3 +11,31 @@
 # #
 # Тут 15 файлов размером не более 100 байт; 3 файла больше 100 и не больше 1000 байт...
 # Подсказка: размер файла можно получить из атрибута .st_size объекта os.stat.
+import os
+
+result = {}
+for root, dirs, files in os.walk('some_data'):
+    for file in files:
+        file_size = os.path.getsize(f'some_data/{file}')
+        if file_size < 100:
+            if 100 in result:
+                result[100] += 1
+            else:
+                result.setdefault(100, 1)
+        elif 100 <= file_size < 1000:
+            if 1000 in result:
+                result[1000] += 1
+            else:
+                result.setdefault(1000, 1)
+        elif 1000 <= file_size < 10000:
+            if 10000 in result:
+                result[10000] += 1
+            else:
+                result.setdefault(10000, 1)
+        elif 10000 <= file_size < 100000:
+            if 100000 in result:
+                result[100000] += 1
+            else:
+                result.setdefault(100000, 1)
+
+print(result)
