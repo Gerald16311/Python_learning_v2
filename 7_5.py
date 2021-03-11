b4d2a9718a3022ad8897223b077ace24ec8cda2d
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-11 21:22:58 +0400

    7_5 в разработке

--- 7_5.py
+++ 7_5.py
@@ -8,3 +8,33 @@
 # }
 # Сохраните результаты в файл <folder_name>_summary.json в той же папке, где запустили скрипт.
 # Задачи со * предназначены для продвинутых учеников, которым мало сделать обычное задание.
+import os
+import json
+
+result = {}
+for root, dirs, files in os.walk('some_data'):
+    for file in files:
+        file_size = os.path.getsize(f'some_data/{file}')
+        if file_size < 100:
+            if 100 in result:
+                result[100] += 1
+            else:
+                result.setdefault(100, 1)
+        elif 100 <= file_size < 1000:
+            if 1000 in result:
+                result[1000] += 1
+            else:
+                result.setdefault(1000, 1)
+        elif 1000 <= file_size < 10000:
+            if 10000 in result:
+                result[10000] += 1
+            else:
+                result.setdefault(10000, 1)
+        elif 10000 <= file_size < 100000:
+            if 100000 in result:
+                result[100000] += 1
+            else:
+                result.setdefault(100000, 1)
+
+print(result)
+
