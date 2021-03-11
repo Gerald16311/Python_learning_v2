adba1c6d1890133d9151e4c3bf8ef78d463cb197
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-11 22:13:53 +0400

    8_1 done?

--- 8_1.py
+++ 8_1.py
@@ -13,3 +13,22 @@
 #
 # Примечание: подумайте о возможных ошибках в адресе и постарайтесь учесть их в регулярном выражении; имеет ли смысл в
 # данном случае использовать функцию re.compile()?
+
+import re
+RE_EMAIL = re.compile(r'^[A-Za-z0-9]{1,10}@[A-Za-z0-9]{2,10}\.[a-z]{2,6}$')
+
+
+def email_parse(email):
+    result = {'username': 'someone', 'domain': 'geekbrains.ru'}
+    assert RE_EMAIL.match(email), f"не правильный email"
+    email_list = email.split("@")
+    result['username'] = email_list[0]
+    result['domain'] = email_list[1]
+    return result
+
+
+print(email_parse('someone@geekbrains.ru'))
+print(email_parse('someone@geekbrainsru'))
+print(email_parse('_someone@geekbr_ains748754.ru4564.com'))
+print(email_parse('someone@@geekbrains.ru'))
+print(email_parse('someone@geekbrains.ru'))
