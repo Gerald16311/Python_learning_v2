8d59a327a138728b797ed9ced47ac3ab6be68c1b
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-13 11:12:40 +0400

    7_5 refactor

--- 7_5.py
+++ 7_5.py
@@ -20,17 +20,18 @@ result_list_100000 = []
 
 for root, dirs, files in os.walk(folder_path):
     for file in files:
-        result_list_100.append(file.split(".")[-1]) if os.path.getsize(f'{folder_path}/{file}') < 100 else ""
-        result_list_1000.append(file.split(".")[-1]) if 100 <= os.path.getsize(f'{folder_path}/{file}') < 1000 else ""
-        result_list_10000.append(file.split(".")[-1]) if 1000 <= os.path.getsize(f'{folder_path}/{file}') < 10000 else ""
-        result_list_100000.append(file.split(".")[-1]) if 10000 <= os.path.getsize(f'{folder_path}/{file}') else ""
+        file_size = os.path.getsize(f'{folder_path}/{file}')
+        file_type = file.split(".")[-1]
+        result_list_100.append(file_type) if file_size < 100 else ""
+        result_list_1000.append(file_type) if 100 <= file_size < 1000 else ""
+        result_list_10000.append(file_type) if 1000 <= file_size < 10000 else ""
+        result_list_100000.append(file_type) if 10000 <= file_size else ""
 
 result[100] = (str(len(result_list_100)), list(set(result_list_100)))
 result[1000] = (str(len(result_list_1000)), list(set(result_list_1000)))
 result[10000] = (str(len(result_list_10000)), list(set(result_list_10000)))
 result[100000] = (str(len(result_list_100000)), list(set(result_list_100000)))
 
-# print(result)
 result_as_str = json.dumps(result)
 with open(f'{folder_path}.json', 'w', encoding='utf-8') as f:
    f.write(result_as_str)
