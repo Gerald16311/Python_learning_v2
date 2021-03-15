bac92611a562e855347d38c4880606ef270f4519
Ilia Marchukov <Gerald16311@gmail.com>
2021-03-15 21:41:04 +0400

    8_3, 8_4 Done

--- 8_4.py
+++ 8_4.py
@@ -17,3 +17,23 @@
 #
 # Примечание: сможете ли вы замаскировать работу декоратора?
 # Задачи со * предназначены для продвинутых учеников, которым мало сделать обычное задание.
+
+def val_checker(lam):
+    def wrapper(func):
+        def inner(arg):
+            if not lam(arg):
+                raise ValueError(f'wrong val {arg}')
+            return func(arg)
+        return inner
+    return wrapper
+
+
+@val_checker(lambda x: x > 0)
+def calc_cube(x):
+    return x ** 3
+
+
+a = calc_cube(5)
+print(a)
+a = calc_cube(-5)
+print(a)
